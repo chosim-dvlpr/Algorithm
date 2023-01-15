e3daee302b2d3684c53a0fbc4989bd6d1b46df58
Minji <121149171+bmj0053@users.noreply.github.com>
2023-01-15 22:11:01 +0900

    create SWEA 1959

--- swea_1959.py
+++ swea_1959.py
@@ -1,24 +1,16 @@
-a = [1,2,3]
-b = [1,3,5,7,9]
-
-min_list = min(len(a),len(b))
-sub_list = abs(len(a)-len(b))
-new_list = []
-my_sum = 0
-
-def length(a, b):
-    for j in range(sub_list+1):
-        if len(a) > len(b):
-            b = [0] + b
-        elif len(a) < len(b):
-            a = [0] + a
-        else:
-            print(a, b)
-
-for i in range(min_list):
-    my_sum += a[i]*b[i]
-new_list.append(my_sum)
-print('new_list :',new_list)
-
-
-
+for i in range(1,int(input())+1):
+    N, M = map(int, input().split())
+    aj = list(map(int, input().split()))
+    bj = list(map(int, input().split()))
+    
+    if len(aj) > len(bj):
+        aj, bj = bj, aj # a를 b로, b를 a로 바꿈
+    
+    new_list = []
+    for k in range(len(bj)-len(aj)+1):
+        sums = 0
+        for z in range(len(aj)):
+            sums += aj[z] * bj[z+k]
+        new_list.append(sums)
+    maxs = max(new_list)
+    print(f'#{i} {maxs}')
