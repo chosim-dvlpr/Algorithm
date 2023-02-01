5bf60d7c77bbc5e36c804e92972162a241111389
Minji <121149171+bmj0053@users.noreply.github.com>
2023-02-02 08:56:03 +0900

    create 4673

--- Baekjoon/4673.py
+++ Baekjoon/4673.py
@@ -1,11 +1,21 @@
-s_list = list(range(1,100))
+# Baekjoon 4673
 
-for num in range(1,10):
-    num_str = str(num)
-    sums = 0
-    for i in range(len(num_str)):
-        sums += int(num_str[i])
-    s = num + sums
-    if s in s_list:
-        s_list.pop(s)
-    print(s_list)
+
+def fn_d(n):
+    n_list = list(map(int, str(n)))
+    gen = sum(n_list) + n
+    return n, gen  # n은 gen의 제너레이터 - n이 없으면 셀프넘버
+
+
+def is_selfnumber(n):
+    n_list = list(map(int, range(1, fn_d(n)[1] + 1)))  # gen까지의 정수 리스트
+    new_list = []  # 제너레이터 n이 있는 숫자들을 넣기 위한 빈 리스트
+    for num in range(1, n + 1):  # num == 1~n
+        new_list.append(fn_d(num)[1])  # new_list에 num의 gen을 추가
+        if new_list[num - 1] in n_list:
+            n_list.remove(new_list[num - 1])
+
+    return n_list
+
+
+print(*is_selfnumber(10000),sep='\n') # 리스트를 한 줄에 하나씩 출력
