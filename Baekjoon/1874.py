59a25eca54e861af9a19350c5549555b27d6a5ba
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2023-03-31 21:42:02 +0900

    modified 1874

--- Baekjoon/1874.py
+++ Baekjoon/1874.py
@@ -13,24 +13,35 @@ push =>순서는 항상 오름차순
 '''
 
 import sys
+sys.stdin = open('input.txt')
 
 n = int(sys.stdin.readline())
-num_list = list(map(int, range(1, n+1)))[::-1]
-lst = [int(sys.stdin.readline()) for _ in range(n)][::-1]
-stack = []
-res = []
-pushpop = []
+num_list = list(map(int, range(1, n+1)))[::-1]              # 구해야 하는 순서
+lst = [int(sys.stdin.readline()) for _ in range(n)][::-1]   # 입력받은 결과값
+
+stack = []      # 숫자를 임시 저장할 스택
+res = []        # res와 num_list가 같아야 함
+pushpop = []    # +와 -를 저장할 리스트
 
 for i in lst:
+    stack.append(i)
     pushpop.append('+') # 모든 값은 push됨
     if i == n:  # i가 최고값일 때
-        res.append(i)
+        res.append(stack.pop())
         pushpop.append('-')
-    while res and res[-1]-1 != i: # res에 값이 들어있고 res의 가장 마지막 값에 1을 뺀 값이 i와 같다면
-        res.append(i)
+
+    # stack에 값이 들어있고 res의 가장 마지막 값에 1을 뺀 값이 스택의 마지막 값과 같다면
+    while stack and res and res[-1]-1 == stack[-1]:
+        res.append(stack.pop())
         pushpop.append('-')
-if not stack or res != lst:
+
+if res != num_list:
     print('NO')
 else:
-    print('YES')
+    # +와 -를 반대로 저장했으므로 반대로 출력
+    for i in pushpop[::-1]:
+        if i == '+':
+            print('-')
+        else:
+            print('+')
 
