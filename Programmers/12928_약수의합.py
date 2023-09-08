8e6412b417feca65556f64976d2ca85341c0640a
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2023-09-08 19:57:04 +0900

    Complete 12928_약수의합.py

--- Programmers/12928_약수의합.py
+++ Programmers/12928_약수의합.py
@@ -1,16 +1,19 @@
 def solution(n):
     answer = 0
-    end = n // 2
-    a = 0
-    b = 0
     
-    while 1:
-        a += 1    
-        if n % a == 0: # 딱 떨어질 때
-            b = n / a        
-            answer += int(a + b)
-        print(a, b, answer)
-        if a == end:
-            break
+    arr = [0] * 3001
+    if n > 1:
+        for i in range(1, n+1):
+            if arr[i] == 1:
+                break
+            elif arr[i] == 0 and n % i == 0: # 딱 떨어질 때
+                if i != n//i: # 제곱수가 아닐때
+                    answer += i + n//i
+                else:
+                    answer += i
+                    break
+                arr[n//i] = 1 # 몫을 1로 바꿈
+    elif n == 1:
+        answer = 1
     
     return answer
