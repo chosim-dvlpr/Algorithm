eaa33085d1d5626b14f10d92d63e4b104040bb5e
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2023-09-13 08:57:15 +0900

    Complete 77884_약수의_개수와_덧셈.py

--- Programmers/77884_약수의_개수와_덧셈.py
+++ Programmers/77884_약수의_개수와_덧셈.py
@@ -8,15 +8,22 @@ def solution(left, right):
         i = 0
         cal = n # i가 순회할 마지막 값
         cnt = 0 # 약수의 개수
-        while i != cal:
-            i += 1
-            print(n, i, cal)
-            if n % i == 0: # 나눠진다면
-                cnt += 1
-                cal = n // i # 최대값을 나눈 몫으로 바꿈
+        
+        if n == 1:
+            cnt = 1
+        else:
+            while i < cal-1:
+                i += 1
+                if n % i == 0: # 나눠지면
+                    if  i ** 2 != n: # 제곱수가 아니라면
+                        cnt += 2
+                    else: # 제곱수일때
+                        cnt += 1 
+                    cal = n // i # 최대값을 나눈 몫으로 바꿈
+
         if cnt % 2: # 약수가 홀수개면
             answer -= n
         else: # 약수가 짝수개면
-            answer += n
+            answer += n        
 
     return answer
