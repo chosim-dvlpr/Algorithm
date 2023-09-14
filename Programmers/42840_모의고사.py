15397ad01dac40cd15bcb20574bcb732a7774d0d
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2023-09-14 09:14:24 +0900

    Create 42840_모의고사.py

---
+++ Programmers/42840_모의고사.py
@@ -0,0 +1,21 @@
+def solution(answers):
+    answer = []
+    pattern = {
+        1: [1, 2, 3, 4, 5],
+        2: [2, 1, 2, 3, 2, 4, 2, 5],
+        3: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    }
+    i = 0
+    cnt_1 = 0
+    cnt_2 = 0
+    cnt_3 = 0
+
+    while i != len(answers):
+        # 1번 학생
+        idx_1 = i % len(pattern[1])
+        
+    
+    
+    
+    
+    return answer
