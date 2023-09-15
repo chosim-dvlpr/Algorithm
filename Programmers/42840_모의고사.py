a4f9c762b2333aa41ed16ee34975a2ddaa2a116c
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2023-09-15 09:57:59 +0900

    Update 42840_모의고사.py

--- Programmers/42840_모의고사.py
+++ Programmers/42840_모의고사.py
@@ -6,15 +6,25 @@ def solution(answers):
         3: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     }
     i = 0
-    cnt_1 = 0
-    cnt_2 = 0
-    cnt_3 = 0
+    cnt = [0] * 4
 
     while i != len(answers):
         # 1번 학생
-        idx_1 = i % len(pattern[1])
+        idx_1 = i % len(pattern[1]) - 1
+        # 2번 학생
+        idx_2 = i % len(pattern[2]) - 1
+        # 3번 학생
+        idx_3 = i % len(pattern[3]) - 1
         
+        for n in range(1, 4):
+            if answers[i] == pattern[n][i]:
+                cnt[n] += 1
+        print(cnt)
+        i += 1
     
+    # 맞춘 개수 비교
+    
+        
     
     
     
