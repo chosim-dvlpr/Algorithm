679f441b38b94bfae4a18f50650dbe3156c79331
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2023-09-17 20:14:31 +0900

    idx 조건 변경 및 풀이 완료

--- Programmers/42840_모의고사.py
+++ Programmers/42840_모의고사.py
@@ -6,35 +6,31 @@ def solution(answers):
         3: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     }
     i = 0
-    cnt = [[1, 0], [2, 0], [3, 0]] # 학생번호, 맞춘 개수
-
+    cnt = [0, 0, 0] # 맞춘 개수
+    
     while i != len(answers):
-        # # 1번 학생
-        # idx_1 = i % len(pattern[1]) - 1
-        # # 2번 학생
-        # idx_2 = i % len(pattern[2]) - 1
-        # # 3번 학생
-        # idx_3 = i % len(pattern[3]) - 1
+        # 1번 학생
+        idx_1 = i % len(pattern[1])
+        # 2번 학생
+        idx_2 = i % len(pattern[2])
+        # 3번 학생
+        idx_3 = i % len(pattern[3])
         
-        for n in range(1, 4):
-            if len(pattern[n]) - 1 == i:
-                idx = i
-            else:
-                idx = i % (len(pattern[n]) - 1)
-            # print(i, idx, n)
-            if answers[i] == pattern[n][idx]:
-                cnt[n-1][1] += 1
+        idx = answers[i]
+        if idx == pattern[1][idx_1]:
+            cnt[0] += 1
+        if idx == pattern[2][idx_2]:
+            cnt[1] += 1
+        if idx == pattern[3][idx_3]:
+            cnt[2] += 1
+        
         i += 1
-    
-    # 맞춘 개수 비교
-    cnt.sort(key=lambda x: x[1], reverse=True)
-    
-    for idx in cnt:
-        if idx[1] == 0:
-            break
-        answer.append(idx[0])
         
+    mx = max(cnt) # cnt 리스트의 최대값
     
+    for i, n in enumerate(cnt):
+        if mx == n: # 가장 높은 점수를 가진 사람만 배열에 추가
+            answer.append(i+1)
     
     
     return answer
