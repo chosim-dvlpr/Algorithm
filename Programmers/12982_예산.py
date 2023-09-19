3feff9dfe5861584cf670cfd7485cb276dd1138c
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2023-09-19 09:48:06 +0900

    12982_예산.py 초기 세팅

---
+++ Programmers/12982_예산.py
@@ -0,0 +1,26 @@
+def solution(d, budget):
+    # d : 예산 리스트
+    # budget : 채워야 하는 예산
+    # 완전탐색
+    # 부분집합
+    
+    d.sort()
+    d_length = len(d)
+    i = 0
+    mx_cnt = 0
+    
+    
+    
+#     while 1: # 시작값
+#         while 1:
+#             if i == (d_length - mx_cnt):
+#                 break
+
+#             cnt = 0 # 지원할 수 있는 부서의 개수
+
+#             for j in range(i, d_length):
+#                 pass
+#             i += 1
+
+    answer = 0
+    return answer
