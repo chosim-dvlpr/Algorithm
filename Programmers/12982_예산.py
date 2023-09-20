fffadb2272661aea9ed3e9e2f51be2f30034fca9
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2023-09-20 09:03:14 +0900

    부분집합 대입

--- Programmers/12982_예산.py
+++ Programmers/12982_예산.py
@@ -2,25 +2,26 @@ def solution(d, budget):
     # d : 예산 리스트
     # budget : 채워야 하는 예산
     # 완전탐색
-    # 부분집합
     
     d.sort()
     d_length = len(d)
-    i = 0
     mx_cnt = 0
+        
+    # 나올 수 있는 모든 부분 집합의 경우의 수 : 2^n (공집합 포함)
+    for i in range(1, 1<<d_length): # i : 1 ~ 2^d_length - 1
+        for j in range(d_length): # j : 0 ~ d_length
+            pass
+        
     
-    
-    
-#     while 1: # 시작값
-#         while 1:
-#             if i == (d_length - mx_cnt):
-#                 break
+#     while 1:
+#         if i == (d_length - mx_cnt):
+#             break
 
-#             cnt = 0 # 지원할 수 있는 부서의 개수
-
-#             for j in range(i, d_length):
-#                 pass
-#             i += 1
+#         cnt = 0 # 지원할 수 있는 부서의 개수
+        
+#         for j in range(i, d_length):
+#             pass
+#         i += 1
 
     answer = 0
     return answer
