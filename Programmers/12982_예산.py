14bd9beb19868d729ab654711b00f71857ca2593
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2023-09-24 23:23:27 +0900

    budget에서 값을 빼는 것으로 변경

--- Programmers/12982_예산.py
+++ Programmers/12982_예산.py
@@ -4,21 +4,17 @@ def solution(d, budget):
     # 완전탐색
     
     d.sort()
-    d_length = len(d)
-    mx_cnt = 0 # 최대 부서 수
-    
-    for i in range(1, 1<<d_length):
-        total = 0 # 비용 합계
-        cnt = 0 # 지원 가능한 부서 수
-        
-        for j in range(d_length):
-            if i & (1<<j): # i의 j번째 위치가 1이라면
-                total += d[j]
-                cnt += 1
-                # print(i, j, cnt)
-                if total > budget:
-                    break
-                if total <= budget and mx_cnt < cnt:
-                    mx_cnt = cnt
+    answer = 0
 
-    return mx_cnt
+    for i, part in enumerate(d):
+        budget -= part
+
+        if budget < 0:
+            answer = i
+            break
+        elif budget == 0:
+            answer = i+1
+            break
+
+                    
+    return answer
