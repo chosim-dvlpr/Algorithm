49abaca523b723777106874c8f02fedb6e42814c
bmj0053 <bmj05378@gmail.com>
2023-09-28 20:28:33 +0900

    rotate 추가

--- Baekjoon/2161.py
+++ Baekjoon/2161.py
@@ -2,7 +2,7 @@ import sys
 from collections import deque
 
 def solution (end):
-  answer = []
+  # answer = []
   # q = deque()
   q = deque(_ for _ in range(1, end+1))
 
@@ -10,20 +10,21 @@ def solution (end):
 
   while 1:
     if flag: # 버리는 동작
-      answer.append(q.popleft())
+      # answer.append(q.popleft())
+      print(q.popleft(), end=" ")
       flag = 0
     else: # 밑으로 내리는 동작
-      q.append(q.popleft())
+      q.rotate(-1)
       flag = 1
 
     # q 길이가 한 개만 남게 되면 answer에 추가
     if len(q) == 1:
-      answer.append(q[0])
+      print(q[0])
       break
-  return answer
+  return 
 
 
 end = int(sys.stdin.readline())
-
-for n in solution(end):
-  print(n, end=" ")
+solution(end)
+# for n in solution(end):
+#   print(n, end=" ")
