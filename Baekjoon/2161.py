e88e8b1ddd007bdee8593633ee70834c0b0f1ef3
bmj0053 <bmj05378@gmail.com>
2023-09-28 20:33:59 +0900

    [Complete] if문을 while 종료 후 추가

--- Baekjoon/2161.py
+++ Baekjoon/2161.py
@@ -8,7 +8,7 @@ def solution (end):
 
   flag = 1 # 버리기
 
-  while 1:
+  while len(q) != 1:
     if flag: # 버리는 동작
       # answer.append(q.popleft())
       print(q.popleft(), end=" ")
@@ -17,10 +17,10 @@ def solution (end):
       q.rotate(-1)
       flag = 1
 
-    # q 길이가 한 개만 남게 되면 answer에 추가
-    if len(q) == 1:
-      print(q[0])
-      break
+  # q 길이가 한 개만 남게 되면 answer에 추가
+  if len(q) == 1:
+    print(q[0])
+    # break
   return 
 
 
