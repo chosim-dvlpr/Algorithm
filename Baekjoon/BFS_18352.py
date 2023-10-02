a51cef4091da71ea98f3249cfd2f24ebaa099fe7
bmj0053 <bmj05378@gmail.com>
2023-10-02 13:47:49 +0900

    [Complete][BFS] 다음위치 = 현재위치+1 방법 추가

--- Baekjoon/BFS_18352.py
+++ Baekjoon/BFS_18352.py
@@ -5,45 +5,33 @@ from collections import deque
 # m : 도로의 개수
 # k : 거리 정보
 # x : 출발 도시의 번호
-# lst : 도로 (단방향)
+# graph : 도로 (단방향)
 
 # x부터 출발하여, 최단거리가 k인 도시를 오름차순으로 출력하기
 # 최단 거리가 k인 도시가 없으면 -1
 # 최단 거리가 k보다 작으면 안됨
 
 n, m, k, x = map(int, sys.stdin.readline().split())
-lst = [[]]            # 도로 정보
-queue = deque()       # 가지치기 기준점
-visited = [0] * (n+1) # 방문 표시
-visited[x] = 1        # 출발하는 도시 먼저 방문 표시
-answer = []           # 최단 거리가 k인 도시들
-cnt = 0               # 거리
+graph = [[] for _ in range(n+1)] # 도로 정보
+distance = [-1] * (n+1) # 도시들의 최단거리 정보
+distance[x] = 0 # 자기 자신으로 가는 거리는 0
+queue = deque([x])    # 가지치기 기준점
 
 for _ in range(m):
-  lst.append(list(map(int, sys.stdin.readline().split())))
+  a, b = map(int, sys.stdin.readline().split())
+  graph[a].append(b)
 
-def bfs():
-  global visited, queue, cnt
+while queue: # queue에 값이 없을 때까지 반복
+  curr = queue.popleft() # 현재 위치
 
-  if not queue:
-    return
+  for next in graph[curr]: # 현재 위치에서 갈 수 있는 정점 순회
+    if distance[next] == -1: # 다음 위치를 방문하지 않았다면
+      distance[next] = distance[curr] + 1 # 다음 위치까지 도달했을 때의 거리 정보 저장
+      queue.append(next) # 다음 위치 정보 저장
 
-  if cnt > k:
-    return
-    
-  s = queue.pop(0) # 가지치기 기준점
-
-  # 인접 확인
-  for r in lst[s]:
-    # 방문하지 않았다면
-    if not visited[r]:
-      queue.append(r)
-      visited[r] = 1
-      cnt += 1
-  
-  bfs() # queue가 빌 때까지 반복
-
-  # return answer
-
-bfs()
-print(cnt)
+if k in distance:
+  for i in range(1, n+1):
+    if distance[i] == k:
+      print(i)
+else:
+  print(-1)
