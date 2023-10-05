51d403bdce1969251784bd2875b509c67a557c5c
bmj0053 <bmj05378@gmail.com>
2023-10-05 22:14:11 +0900

    [Complete] 이진 정렬 속도 빠른 풀이 추가

--- Baekjoon/Binary_Search_2110.py
+++ Baekjoon/Binary_Search_2110.py
@@ -18,26 +18,54 @@ for _ in range(n):
     lst.append(int(input()))
 lst.sort()
 
-start = 1 # 최소 거리
-end = lst[-1] - lst[0] # 최대 거리
-result = 0
+# [방법 2]
+def bin_search(mid):
+    # mid : 가장 인접한 두 공유기 사이의 거리
+    cnt, value = 0, lst[0]
+
+    # lst 값들 : 공유기가 있는 위치
+    for i in lst:
+        if value > i: # 직전 공유기의 값이 lst값보다 클 때
+            continue
+        else:         # 직전 공유기의 값이 lst값보다 작거나 같을 때
+            value = i + mid # 직전 공유기의 값을 lst값 + mid값으로 변경
+            cnt += 1
+    return cnt
+
+wifiCnt = result = 0
+start, end = 1, lst[-1] - lst[0] # 시작, 끝 값
 
 while start <= end:
-    mid = (start + end) // 2 # 가장 인접한 두 공유기 사이의 거리
-    value = lst[0]
-    count = 1
-    
-    # 앞에서부터 차근차근 공유기 설치
-    for i in range(1, n):
-        if lst[i] >= value + mid: # value와 두 공유기 사이 거리 합보다 클 때
-            value = lst[i] # value 값 업데이트
-            count += 1
-    
-    # c개 이상의 공유기를 설치할 수 있는 경우, 공유기 사이 거리를 증가시켜 순회
-    if count >= c:
-        start = mid + 1 # 가능한 최소 거리를 가장 인접한 두 공유기 사이 거리 +1로 설정
-        result = mid # 현재까지의 최적 결과를 저장
-    else: # c개 이상의 공유기를 설치할 수 없는 경우, 거리를 감소
-        end = mid - 1
+    mid = (start + end) // 2
+    wifiCnt = bin_search(mid)
 
+    if wifiCnt >= c:
+        start = mid + 1
+        result = mid
+    else:
+        end = mid - 1
 print(result)
+
+# start = 1 # 최소 거리
+# end = lst[-1] - lst[0] # 최대 거리
+# result = 0
+
+# while start <= end:
+#     mid = (start + end) // 2 # 가장 인접한 두 공유기 사이의 거리
+#     value = lst[0]
+#     count = 1
+    
+#     # 앞에서부터 차근차근 공유기 설치
+#     for i in range(1, n):
+#         if lst[i] >= value + mid: # value와 두 공유기 사이 거리 합보다 클 때
+#             value = lst[i] # value 값 업데이트
+#             count += 1
+    
+#     # c개 이상의 공유기를 설치할 수 있는 경우, 공유기 사이 거리를 증가시켜 순회
+#     if count >= c:
+#         start = mid + 1 # 가능한 최소 거리를 가장 인접한 두 공유기 사이 거리 +1로 설정
+#         result = mid # 현재까지의 최적 결과를 저장
+#     else: # c개 이상의 공유기를 설치할 수 없는 경우, 거리를 감소
+#         end = mid - 1
+
+# print(result)
