543d2fd84c013c3a725a0bbcb84daa588ccb1bea
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2023-10-06 17:00:16 +0900

    BFS 업데이트 - 시간초과

--- Baekjoon/BFS_14502.py
+++ Baekjoon/BFS_14502.py
@@ -18,31 +18,28 @@ well = [[0]*m for _ in range(n)] # 벽 세운 뒤의 배열
 for _ in range(n):
   arr.append(list(map(int, input().split())))
 
-# visited = [[0]*m for _ in range(n)] # 방문 표시
-# target = [] # 바이러스가 있는 위치
 delta = [(0, 1), (0, -1), (1, 0), (-1, 0)] # 방향
 cnt = 0 # 새로 세운 벽의 개수
 result = 0 # 안전지대 개수
-# curr = [0, 0] # 시작 위치
 
 # 바이러스 전염
-def virus(nx, ny):
+def virus(x, y):
     for d in delta:
-        nx = nx + d[0]
-        ny = ny + d[1]
-        for i in range(n):
-            for j in range(m):
-                    if arr[i][j] == 0:
-                        arr[i][j] = 2 # 바이러스 감염
-                        virus(nx, ny)
+        nx = x + d[0]
+        ny = y + d[1]
+        if 0 <= nx and nx < n and 0 <= ny and ny < m:
+            if well[nx][ny] == 0:
+                well[nx][ny] = 2 # 바이러스 감염
+                virus(nx, ny)
 
 # 안전지대 개수 확인
 def safe_zone():
+    safe = 0
     for i in range(n):
         for j in range(m):
-            if arr[i][j] == 0:
-                result += 1
-    return result
+            if well[i][j] == 0:
+                safe += 1
+    return safe
 
 # 벽 세우기
 def add_well(cnt):
@@ -50,25 +47,30 @@ def add_well(cnt):
 
     # 종료조건
     if cnt == 3:
+        # 배열 업데이트
         for i in range(n):
             for j in range(m):
-                if arr[i][j] == 0:
-                    result += 1
-                    print("arr[i][j] == 0 : ", result)
+                well[i][j] = arr[i][j]
+        
+        # 바이러스 전파하기
+        for i in range(n):
+            for j in range(m):
+                if well[i][j] == 2:
+                    virus(i, j)
         result = max(result, safe_zone())
-        print("cnt == 3 : ", result)
+        return
     
-    # 아직 벽을 세 개 미만 추가했을 때
+    # 아직 벽을 세 개 미만 추가했을 때 새로 벽을 세움
     for i in range(n):
         for j in range(m):
             if arr[i][j] == 0:
-                arr[i][j] = 1
+                arr[i][j] = 1   # 벽을 세움
                 add_well(cnt+1)
-                arr[i][j] = 0
-                add_well(cnt)
+                arr[i][j] = 0   # 원 상태로 복귀
+                # add_well(cnt)
 
 add_well(0)
-
+print(result)
 
 
 # # 바이러스 위치 확인
