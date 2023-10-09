49daf1053dd183f94ddf6f265df39d2d3fa383bf
bmj0053 <bmj05378@gmail.com>
2023-10-09 22:16:12 +0900

    [Complete] global res 선언 추가

--- Baekjoon/Backtracking_15649.py
+++ Baekjoon/Backtracking_15649.py
@@ -1,34 +1,26 @@
 # n까지의 자연수 중 중복 없이 m개를 고른 수열
 
 n, m = map(int, input().split())
+lst = [_ for _ in range(1, n+1)]
 visited = [0] * (n+1)
 res = []
 
-# def bt(idx, visited):
-#     global res
-#     if idx == m:
-#         print(res)
-#         return
-    
-#     for i in range(n):
-#         if not visited[i]:
-#             visited[i] = 1
-#             res.append(i+1)
-#             bt(idx+1, visited)
-#             visited[i] = 0
+def bt(idx, visited):
+    global res
 
-def bt(res, idx, visited):
+    # idx가 뽑아야 하는 개수와 같아질 때
     if idx == m:
-        if len(res) == m:
+        if len(res) == m: # 출력할 리스트 개수가 m과 같을 때 출력
             print(*res)
         return
     
-    for i in range(n):
-        if not visited[i]:
+    for i in range(1, n+1):
+        if not visited[i]: # 방문하지 않았다면 방문표시
             visited[i] = 1
-            
-            bt(res.append(i+1), idx+1, visited)
-            visited[i] = 0
+            res.append(i)  # res에 추가해두고
+            bt(idx+1, visited) # 재귀
+            res.pop()      # res 원상복구
+            visited[i] = 0 # 방문표시 원상복구
 
 
-bt(res, 0, visited)
+bt(0, visited)
