29a3592356bc361f2754526a85a5ab2eb7296712
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2023-10-11 09:22:47 +0900

    인덱스 조건 및 종료 조건 변경

--- Baekjoon/Backtracking_9663.py
+++ Baekjoon/Backtracking_9663.py
@@ -4,27 +4,32 @@
 n = int(input())
 arr = [[0]*n for _ in range(n)]
 delta = [(-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1), (-1, 1), (0, 1), (1, 1)]
-cnt = 0
+res = 0
 
-def bt(row, col):
-  global cnt
+def bt(row, col, cnt):
+  global res
+  # print("cnt : ", cnt)
 
-  if row == n and col == n:
+  if cnt == n:
     print(cnt)
     return
   
   for i in range(row, n):
     for j in range(col, n):
       for d in delta:
-        print(i, j, d)
+        # print(i, j, d)
         nx, ny = i+d[0], j+d[1]
-        if 0 <= nx and nx < n and 0 <= ny and ny < n and arr[nx][ny] == 1:
-          # arr[i][j] = 0
-          bt(i, j)
-        else:
-          arr[i][j] = 1 # queen의 위치
-          cnt += 1
+        if 0 <= nx and nx < n and 0 <= ny and ny < n :
+          # print("여기")
+          if arr[nx][ny] == 0:
+            arr[i][j] = 1 # queen의 위치
+            res += 1
+            bt(i+1, j+1, cnt+1)
+            res -= 1
+          else:
+            arr[i][j] = 0
+            break
 
 
-bt(0, 0)
+bt(0, 0, 0)
 # print(arr)
