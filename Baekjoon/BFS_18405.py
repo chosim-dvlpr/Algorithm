af873b9feecc97fd88ebcefb8a383b649726deb2
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2023-10-12 17:45:40 +0900

    바이러스 위치 찾는 함수, 전염시키는 함수 추가

--- Baekjoon/BFS_18405.py
+++ Baekjoon/BFS_18405.py
@@ -17,6 +17,7 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 arr = deque([])
+temp = [[0]*n for _ in range(n)] # 증식된 뒤의 배열
 
 for _ in range(n):
     arr.append(list(map(int, input().split())))
@@ -25,21 +26,48 @@ x -= 1
 y -= 1
 delta = [(1,0), (-1,0), (0,1), (0,-1)]
 
-start_virus = deque([])
-for i in range(n):
-    for j in range(n):
-        if arr[i][j] > 0:
-            start_virus.append((i, j, arr[i][j]))
+# 바이러스의 위치 찾기
+def find_virus(x, y):
+    virus_list = deque([])
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] > 0:
+                virus_list.append((i, j, arr[i][j])) # (x, y, 바이러스 종류)
+    return virus_list
 
-def bfs(sec):
-    while start_virus:
-        s = start_virus.pop()
+# 바이러스 전염
+def virus(x, y, k):
+    for d in delta:
+        nx = x + d[0]
+        ny = y + d[1]
+        if 0 <= nx and nx < n and 0 <= ny and ny < n:
+            if temp[nx][ny] == 0:
+                temp[nx][ny] = k
+                virus(nx, ny, k)
+
+
+
+
+def bfs(x, y, sec):
+    virus_list = find_virus(x, y)
+    print(x, y, sec, virus_list)
+    # s초가 지났을 때
+    if sec == s:
+        print(temp[x][y]) # (x, y)위치에 있는 바이러스 종류 출력
+        return
+    
+    while virus_list:
+        start = virus_list.pop()
+        print("=====", start)
 
         for d in delta:
-            nx, ny = s[0] + d[0], s[1] + d[1]
+            nx, ny = start[0] + d[0], start[1] + d[1]
             if 0 <= nx and nx < n and 0 <= ny and ny < n:
-                if arr[nx][ny] == 0: # 칸에 바이러스가 퍼지지 않았을 때만
-                    arr[nx][ny] = s[2]
+                bfs(nx, ny, sec+1)
+                # if arr[nx][ny] == 0: # 칸에 바이러스가 퍼지지 않았을 때만
+                #     arr[nx][ny] = s[2]
+                # bfs(sec+1)
                     
 
+bfs(0, 0, 0)
 # print(arr)
