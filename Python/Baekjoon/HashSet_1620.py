dd4d43386ffc47c308a10366ca4c4cd686e72109
bmj0053 <bmj05378@gmail.com>
2023-10-21 11:40:42 +0900

    [해시셋][Create] 시간초과

---
+++ Python/Baekjoon/HashSet_1620.py
@@ -0,0 +1,24 @@
+# n : 도감에 수록된 포켓몬의 개수
+# m : 맞춰야 하는 문제의 개수
+# 포켓몬의 이름 : 영어, 첫 글자만 대문자 (2~20)
+# 일부 포켓몬은 마지막 문자만 대문자
+# 알파벳으로 들어오면 포켓몬 번호 출력
+# 숫자로 들어오면 문자 출력
+
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+lst = []
+
+for _ in range(n):
+  lst.append(input().strip())
+
+for _ in range(m):
+  quiz = input().strip()
+
+  if quiz.isnumeric():
+    print(lst[int(quiz)-1])
+  else:
+    print(lst.index(quiz)+1)
