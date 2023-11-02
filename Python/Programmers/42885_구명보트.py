682db834606df23935b2d6a6462cc5e010950791
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2023-11-02 21:40:07 +0900

    [Greedy][Complete]

--- Python/Programmers/42885_구명보트.py
+++ Python/Programmers/42885_구명보트.py
@@ -1,10 +1,20 @@
+# 가장 무거운 사람 + 가장 가벼운 사람 한 배에 태우는게 가장 이득
+# people 정렬 후 0번째와 -1번째 사람을 한 배에 태움
+# limit 넘어간다면 무거운 사람을 먼저 pop
+
+from collections import deque
+
 def solution(people, limit):
     answer = 0
-    l = len(people)
-    arr = [0] * l
+    people.sort()
+    people = deque(people)
     
-    for i, p in enumerate(people):
-        
+    while len(people) > 1:
+        if people[0] + people[-1] <= limit:
+            people.popleft()
+        answer += 1
+        people.pop()
     
-    
+    if people:
+        answer += 1
     return answer
