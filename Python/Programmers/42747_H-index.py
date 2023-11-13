49f3f1dea95c9e2c5cfe1edf58064f23158ba269
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2023-11-13 20:08:35 +0900

    [정렬][Complete] 배열을 거꾸로 순회

--- Python/Programmers/42747_H-index.py
+++ Python/Programmers/42747_H-index.py
@@ -1,13 +1,9 @@
 def solution(citations):
+    citations.sort(reverse = True)
     answer = 0
-    citations.sort()
-    
-    for i, c in enumerate(citations):
-        if len(citations[i:]) == c:
-            answer = citations[i]
+    for i in range(len(citations)): # 인용횟수가 많은 논문부터 순회
+        if citations[i] > answer: # 만약 i번째로 인용횟수가 많은 논문의 횟수가 현재 h-index(answer)보다 많다면
+            answer = i+1 # 순회를 돈 논문 개수
+        else:
             break
-        elif len(citations[i:]) < c:
-            answer = citations[i-1]
-            break
-    
     return answer
