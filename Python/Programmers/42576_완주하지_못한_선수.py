3676bf625d6151c9e094bed52d5fc335e00150ed
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2023-11-19 00:04:08 +0900

    [Hash][Complete]

--- Python/Programmers/42576_완주하지_못한_선수.py
+++ Python/Programmers/42576_완주하지_못한_선수.py
@@ -1,12 +1,19 @@
 def solution(participant, completion):
     dic = {}
-    for c in completion:
-        dic[c] = 0
-    
     for p in participant:
         temp = dic.get(p, -1)
+        # 아직 key로 안들어가있을 때, 중복이 아닌 상태
         if temp == -1:
-            return p
-        dic[p] += 1
+            dic[p] = 1
+        # key로 이미 들어가있을 때, 중복문자
+        else:
+            dic[p] += 1
+    
+    for c in completion:
+        dic[c] -= 1
+    
+    for d in dic.keys():
+        if dic[d] > 0:            
+            return d
         
     return 
