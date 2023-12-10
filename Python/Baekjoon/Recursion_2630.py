0989cf32bff0bc5723791b10d949a640111e95af
bmj0053 <bmj05378@gmail.com>
2023-12-10 23:27:26 +0900

    [재귀][Complete]

--- Python/Baekjoon/Recursion_2630.py
+++ Python/Baekjoon/Recursion_2630.py
@@ -1,40 +1,41 @@
+# 종이를 반으로 자르는 것을 반복
+# 모두 같은 색으로 칠해져 있거나 한 칸이 되어 더 이상 자를 수 없게 되면 종료
+
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 # 0 : 하얀색
 # 1 : 파란색
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
-delta = [(0, 1), (1, 0)]
-visited = [[0]*n for _ in range(n)]
-
-def bfs(x, y, num):
-    q = deque([(x, y)])
-    visited[x][y] = 1
-    while q:
-        x, y = q.popleft()
-        for d in delta:
-            nx, ny = x+d[0], y+d[1]
-            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and arr[nx][ny] == arr[x][y]:
-                q.append((nx, ny))
-                visited[nx][ny] = 1
-
 white = 0
 blue = 0
-# visited = [[0]*n for _ in range(n)]
-for i in range(n):
-    for j in range(n):
-        if not visited[i][j] and arr[i][j] == 0:
-            print('0 : ', i, j)
-            bfs(i, j, 0)
-            white += 1
-        if not visited[i][j] and arr[i][j] == 1:
-            print('1 : ', i, j)
-            bfs(i, j, 0)
-            blue += 1
+def recursion(start, end):
+    global white, blue
+    length = end[0] - start[0] + 1
 
+    if length <= 1:
+        if arr[start[0]][start[1]] == 1:
+            blue += 1
+            return
+        white += 1
+        return
+
+    color = arr[start[0]][start[1]]
+    for i in range(start[0], end[0]+1):
+        for j in range(start[1], end[1]+1):
+            # 색이 다른 부분이 있다면 종이를 4등분하여 재귀
+            if arr[i][j] != color:
+                recursion((start[0], start[1]), (start[0]+length//2-1, start[1]+length//2-1))
+                recursion((start[0], start[1]+length//2), (start[0]+length//2-1, start[1]+length-1))
+                recursion((start[0]+length//2, start[1]), (start[0]+length-1, start[1]+length//2-1))
+                recursion((start[0]+length//2, start[1]+length//2), (start[0]+length-1, start[1]+length-1))
+                return
+    if arr[start[0]][start[1]] == 1:
+        blue += 1
+        return
+    white += 1
+
+recursion((0, 0), (n-1, n-1))
 print(white)
 print(blue)
-                
-        
