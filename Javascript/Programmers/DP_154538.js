cd74030213e024a676f73cd2e12587eb15ef6b1c
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2023-12-11 22:23:06 +0900

    [JS][DP][Complete]

---
+++ Javascript/Programmers/DP_154538.js
@@ -0,0 +1,16 @@
+// bfs => 시간초과 => dp로 풀기
+
+function solution(x, y, n) {
+    const dp = new Array(y+1).fill(Infinity);
+    dp[x] = 0;
+    for (let i = x; i <= y; i++) {
+        dp[i+n] = Math.min(dp[i+n], dp[i]+1);
+        dp[i*2] = Math.min(dp[i*2], dp[i]+1);
+        dp[i*3] = Math.min(dp[i*3], dp[i]+1);
+    }
+    
+    if (dp[y] !== Infinity) {
+        return dp[y]
+    }
+    return -1
+}
