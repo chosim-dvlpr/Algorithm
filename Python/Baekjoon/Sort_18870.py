656408edb58405c87e0e6ed204401bc2d4dc5337
bmj0053 <bmj05378@gmail.com>
2023-12-30 22:28:34 +0900

    [정렬][Complete] 두 번째 풀이 추가

--- Python/Baekjoon/Sort_18870.py
+++ Python/Baekjoon/Sort_18870.py
@@ -1,6 +1,7 @@
 import sys
 input = sys.stdin.readline
 
+# 풀이 [1] : 이진탐색 사용
 n = int(input())
 lst = list(map(int, input().split()))
 data = sorted(list(set(lst)))
@@ -22,3 +23,11 @@ def bin_search(target):
 
 for i in lst:
     print(bin_search(i), end=' ')
+
+# 풀이 [2] : dict 사용
+n = int(input())
+lst = list(map(int, input().split()))
+sorted_lst = sorted(list(set(lst)))
+dic = {sorted_lst[i] : i for i in range(len(sorted_lst))} # 정렬한 인덱스 번호 저장 == 나보다 작은 원소의 수
+for i in lst:
+    print(dic[i], end=' ')
