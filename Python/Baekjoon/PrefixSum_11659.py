3f4b5299add9815c69e7a6a60c2ff5b6f1066683
bmj0053 <bmj05378@gmail.com>
2024-01-06 15:41:19 +0900

    [누적합][DP][Complete]

--- Python/Baekjoon/PrefixSum_11659.py
+++ Python/Baekjoon/PrefixSum_11659.py
@@ -1,12 +1,27 @@
 # n개의 수
 # i번째 수부터 j번째 수까지 합을 구하기
 # m : 합을 구해야 하는 횟수
+# DP?
+
+
 import sys
+input = sys.stdin.readline
 
-n, m = map(int, sys.stdin.readline().split())
-
-# 문제에서 i, j가 인덱스 + 1이므로 arr 앞에 0을 더해줌
-arr = [0] + list(map(int, sys.stdin.readline().split()))
+n, m = map(int, input().split())
+lst = list(map(int, input().split()))
+arr = [0]*n
+arr[0] = lst[0]
+for i in range(1, n):
+    arr[i] = arr[i-1] + lst[i]
 
 for _ in range(m):
-  i, j = map(int, sys.stdin.readline().split())
+    i, j = map(int, input().split())
+    # 인덱스로 변경
+    i -= 1
+    j -= 1
+    if i != 0:
+        print(arr[j] - arr[i-1])
+        continue
+    print(arr[j])
+
+
