476eb65fde144c98af9323b4e15842611763c5c2
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2025-10-31 00:23:48 +0900

    [Bronze V] Title: Hello World, Time: 100 ms, Memory: 9452 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.js
@@ -0,0 +1,11 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+const input = []
+rl.on('line', (line) => {
+  input.push(line.trim())
+}).on('close', () => {console.log('Hello World!')})
